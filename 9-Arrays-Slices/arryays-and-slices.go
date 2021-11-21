1155e56c55efa487151d9a826a524993fa403420
Rafael <rafael_unis@hotmail.com>
2021-11-21 19:15:05 -0300

    Internal arrays

--- 9-Arrays-Slices/arryays-and-slices.go
+++ 9-Arrays-Slices/arryays-and-slices.go
@@ -8,6 +8,7 @@ import (
 func main() {
 
 	// Arrays
+	fmt.Println("-------------- Arrays ---------------")
 	var array [5]string
 	fmt.Println(array)
 
@@ -20,7 +21,8 @@ func main() {
 	arry3 := [...]int{1, 2, 3, 4, 5, 6, 7}
 	fmt.Println(arry3)
 
-	//Slices
+	// Slices
+	fmt.Println("-------------- Slices ---------------")
 	slice := []int{10, 11, 12, 13, 14, 15, 16, 17}
 	fmt.Println((slice))
 
@@ -35,4 +37,28 @@ func main() {
 
 	array2[1] = "Altered position"
 	fmt.Println(slice2)
+
+	// Internal Arrays
+	fmt.Println("------------ Internal Arrays --------------")
+	slice3 := make([]float32, 10, 11) // Las parameter is optional
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // length
+	fmt.Println(cap(slice3)) // Capacity
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
 }
