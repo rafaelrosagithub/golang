1ba28b799a65db7d4562d6536476379b2f5b6886
Rafael <rafael_unis@hotmail.com>
2022-01-11 21:22:31 -0300

    Wait Group

---
+++ 18-Competition/waitgroup/waitgroup.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var waitGroup sync.WaitGroup
+
+	waitGroup.Add(2)
+
+	go func() {
+		write("Hello World!")
+		waitGroup.Done()
+	}()
+
+	go func() {
+		write("Programming in Go!")
+		waitGroup.Done()
+	}()
+
+	waitGroup.Wait()
+}
+
+func write(text string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(text)
+		time.Sleep(time.Second)
+	}
+}
